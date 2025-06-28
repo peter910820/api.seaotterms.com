d3f75c445c98ae9a68eddb3f76606950721e1ab1
peter910820 <peter910820@gmail.com>
2025-06-28 23:49:21 +0800

    feat: make write log function for gal site

--- utils/gal/db_handler.go
+++ utils/gal/db_handler.go
@@ -24,3 +24,18 @@ func WriteTmpData(dataType string, dataContent string, expirationAt time.Time, d
 
 	return nil
 }
+
+func WriteLog(dataType string, message string, severity uint, db *gorm.DB) error {
+	data := model.Log{
+		Type:     dataType,
+		Message:  message,
+		Severity: severity,
+	}
+
+	err := db.Create(&data).Error
+	if err != nil {
+		logrus.Error(err)
+		return err
+	}
+	return nil
+}
