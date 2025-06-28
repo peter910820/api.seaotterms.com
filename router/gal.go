a4a8218f9cdeb3f87e0becddac8ebc25b4b98cab
peter910820 <peter910820@gmail.com>
2025-06-28 16:29:42 +0800

    chore: set up all site database to be completely independent

--- router/gal.go
+++ router/gal.go
@@ -11,21 +11,26 @@ import (
 
 func GalRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
 	galGroup := apiGroup.Group("/gal")
+	dbName := os.Getenv("DATABASE_NAME2")
 
-	authRouter(galGroup, dbs)
-	loginRouter(galGroup, dbs)
+	authRouter(galGroup, dbs, dbName)
+	loginRouter(galGroup, dbs, dbName)
 	testRouter(galGroup)
 }
 
-func authRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
+func authRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB, dbName string) {
 	apiGroup.Post("/register", func(c *fiber.Ctx) error {
-		return galapi.Register(c, dbs[os.Getenv("DATABASE_NAME2")])
+		return galapi.Register(c, dbs[dbName])
+	})
+
+	apiGroup.Get("/register/:register-key", func(c *fiber.Ctx) error {
+		return galapi.RegisterKeyCheck(c, dbs[dbName])
 	})
 }
 
-func loginRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
+func loginRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB, dbName string) {
 	apiGroup.Post("/login", func(c *fiber.Ctx) error {
-		return galapi.Register(c, dbs[os.Getenv("DATABASE_NAME2")])
+		return galapi.Register(c, dbs[dbName])
 	})
 }
 
--- router/teach.go
+++ router/teach.go
@@ -11,35 +11,36 @@ import (
 
 func TeachRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
 	teachGroup := apiGroup.Group("/teach")
+	dbName := os.Getenv("DATABASE_NAME")
 
-	seriesRouter(teachGroup, dbs)
-	articleRouter(teachGroup, dbs)
-	commentApiRouter(teachGroup, dbs)
+	seriesRouter(teachGroup, dbs, dbName)
+	articleRouter(teachGroup, dbs, dbName)
+	commentApiRouter(teachGroup, dbs, dbName)
 }
 
-func seriesRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
+func seriesRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB, dbName string) {
 	apiGroup.Get("/series", func(c *fiber.Ctx) error {
-		return teachapi.QuerySeries(c, dbs[os.Getenv("DATABASE_NAME")])
+		return teachapi.QuerySeries(c, dbs[dbName])
 	})
 
 	apiGroup.Post("/series", func(c *fiber.Ctx) error {
-		return teachapi.CreateSeries(c, dbs[os.Getenv("DATABASE_NAME")])
+		return teachapi.CreateSeries(c, dbs[dbName])
 	})
 
 	apiGroup.Patch("/series/:id", func(c *fiber.Ctx) error {
-		return teachapi.ModifySeries(c, dbs[os.Getenv("DATABASE_NAME")])
+		return teachapi.ModifySeries(c, dbs[dbName])
 	})
 }
 
-func articleRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
+func articleRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB, dbName string) {
 	apiGroup.Get("/article", func(c *fiber.Ctx) error {
-		return teachapi.QueryArticle(c, dbs[os.Getenv("DATABASE_NAME")])
+		return teachapi.QueryArticle(c, dbs[dbName])
 	})
 
 	apiGroup.Post("/article", func(c *fiber.Ctx) error {
-		return teachapi.CreateArticle(c, dbs[os.Getenv("DATABASE_NAME")])
+		return teachapi.CreateArticle(c, dbs[dbName])
 	})
 }
 
-func commentApiRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB) {
+func commentApiRouter(apiGroup fiber.Router, dbs map[string]*gorm.DB, dbName string) {
 }
