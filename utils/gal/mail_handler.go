40c38311e7cc722df9f7d50d10ca572f45dd6cef
GreenTeaOnly <luo2257@gmail.com>
2025-06-29 21:34:22 +0800

    chore: modify mail send display

--- utils/gal/mail_handler.go
+++ utils/gal/mail_handler.go
@@ -28,34 +28,26 @@ func SendRegisterEmail(to string, registerKey string) {
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", "註冊驗證信件")
 	m.SetBody("text/html", fmt.Sprintf(`			
-		<body style="
-		margin: 0;
-		padding: 0;
-		height: 100vh;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		background-image: url('https://fs.seaotterms.com/resource/image/photo-1745874864678-f464940bb513.JPG');
-		background-size: cover;
-		background-position: center;
-		background-repeat: no-repeat;
-		text-align: center;
-		color: white;
-		font-family: sans-serif;
-		text-shadow: 1px 1px 5px black;
-		">
+		<body style="margin: 0; padding: 0; height: 100vh; display: flex; justify-content: center; align-items: center; position: relative; overflow: hidden;">
 
-		<div style="
-			background-color: rgba(0, 0, 0, 0.5);
-			padding: 40px;
-			border-radius: 15px;
-		">
-			<h1>信箱註冊成功</h1>
-			<p>您的信箱已經註冊成功!</p>
-			<p><a href="%s" style="color: yellow; font-weight: bold; text-decoration: underline;">點擊這裡</a> 完成驗證。</p>
-		</div>
+  <img src="https://fs.seaotterms.com/resource/image/premium_photo-1673029927496-a9b574091d12.jpg"
+       style="position: absolute; width: 100%%; height: auto; top: 50%%; left: 50%%; transform: translate(-50%%, -50%%); opacity: 0.3; z-index: -1;">
+  
+  <div style="
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 40px;
+    border-radius: 15px;
+    color: white;
+    text-align: center;
+    font-family: sans-serif;
+    text-shadow: 1px 1px 5px black;
+  ">
+    <h1>信箱註冊成功</h1>
+    <p>您的信箱已經註冊成功!</p>
+    <p><a href="%s" style="color: yellow; font-weight: bold; text-decoration: underline;">點擊這裡</a> 完成驗證。</p>
+  </div>
 
-		</body>
+</body>
 	`, registerUrl))
 
 	d := gomail.NewDialer(os.Getenv("MAIL_HOST"), 587, os.Getenv("MAIL_SEND_NOREPLY_ACCOUNT"), os.Getenv("MAIL_SEND_NOREPLY_PASSWORD"))
