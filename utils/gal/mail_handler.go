4c90731f86385113c479d11b204a1f030671cb82
GreenTeaOnly <luo2257@gmail.com>
2025-06-29 21:55:39 +0800

    chore: modify mail send display

--- utils/gal/mail_handler.go
+++ utils/gal/mail_handler.go
@@ -28,11 +28,9 @@ func SendRegisterEmail(to string, registerKey string) {
 	m.SetHeader("To", to)
 	m.SetHeader("Subject", "註冊驗證信件")
 	m.SetBody("text/html", fmt.Sprintf(`			
-		<body style="margin: 0; padding: 0; height: 100vh; display: flex; justify-content: center; align-items: center; position: relative; overflow: hidden;">
-
+	<body style="margin: 0; padding: 0; height: 100vh; display: flex; justify-content: center; align-items: center; position: relative; overflow: hidden;">
   <img src="https://fs.seaotterms.com/resource/image/premium_photo-1673029927496-a9b574091d12.jpg"
        style="position: absolute; width: 100%%; height: auto; top: 50%%; left: 50%%; transform: translate(-50%%, -50%%); opacity: 0.3; z-index: -1;">
-  
   <div style="
     background-color: rgba(0, 0, 0, 0.6);
     padding: 40px;
@@ -46,7 +44,6 @@ func SendRegisterEmail(to string, registerKey string) {
     <p>您的信箱已經註冊成功!</p>
     <p><a href="%s" style="color: yellow; font-weight: bold; text-decoration: underline;">點擊這裡</a> 完成驗證。</p>
   </div>
-
 </body>
 	`, registerUrl))
 
